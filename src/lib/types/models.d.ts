de732568a85cc4e7b91134b6c1eb7b7fae3068d4
Edward Nguyen <nhanna@northstudio.vn>
2024-02-11 02:16:15 +0700

    :zap: account model

--- src/lib/types/models.d.ts
+++ src/lib/types/models.d.ts
@@ -1,9 +1,19 @@
+
+type AccountStatus = 'not_verified' | 'verified' | 'banned';
 interface Account {
   username: string;
-  email: string;
-  password?: string;
-  birthday: string;
-  joinDate?: Date;
+  birthday: Date;
   role: import('@/lib/models/account.model').RoleEnum;
   googleId?: string;
+  fullName: string;
+  email: string;
+  phoneNumber: string;
+  identityNumber: string;
+  password: string;
+  gender: string;
+  joinDate: Date;
+  profilePicture: string;
+  balance: number;
+  address: string;
+  status: AccountStatus;
 }
