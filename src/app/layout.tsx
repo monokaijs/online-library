340161b9db00585cdfee36acd1a205ba15c9470c
dephrish <dephrish@gmail.com>
2024-02-16 22:10:18 +0700

    import antd utils class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./global.css";
 import {ReactNode} from "react";
 import ThemeProvider from "@/components/providers/ThemeProvider";
 import {AntdRegistry} from "@ant-design/nextjs-registry";
+import 'antd-css-utilities/utility.min.css';
 
 export default function RootLayout({children}: Readonly<{ children: ReactNode }>) {
   return (
