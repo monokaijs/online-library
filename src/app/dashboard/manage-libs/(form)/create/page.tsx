63859f03d193758b696ee44255595593638372f3
dephrish <dephrish@gmail.com>
2024-02-17 21:56:36 +0700

    Fix UI Lib Form

--- src/app/dashboard/manage-libs/(form)/create/page.tsx
+++ src/app/dashboard/manage-libs/(form)/create/page.tsx
@@ -2,5 +2,8 @@ import LibForm from "@/app/dashboard/manage-libs/(form)/components/LibForm";
 import { Card } from "antd";
 
 export default function CreateLibPage() {
-	return <Card bordered={false}><LibForm/></Card>;
+	return <Card bordered={false} style={{
+		maxWidth: 714,
+		margin: "0 auto"
+	}}><LibForm/></Card>;
 }
