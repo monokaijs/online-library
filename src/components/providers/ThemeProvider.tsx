03f258360d4b9201181a368c604b0b2a83d07026
dephrish <dephrish@gmail.com>
2024-02-17 21:48:52 +0700

    Config theme provider

--- src/components/providers/ThemeProvider.tsx
+++ src/components/providers/ThemeProvider.tsx
@@ -1,25 +1,38 @@
-import {ConfigProvider} from "antd";
-import {ReactNode} from "react";
-import {Lexend_Deca} from "next/font/google";
+import { ConfigProvider } from "antd";
+import { ReactNode } from "react";
+import { Lexend_Deca } from "next/font/google";
 
 interface ThemeProviderProps {
-  children: ReactNode;
+	children: ReactNode;
 }
 
 const lexendDeca = Lexend_Deca({
-  weight: ['300', '400', '500', '600'],
-  subsets: ['latin'],
-})
+	weight: ['300', '400', '500', '600'],
+	subsets: ['latin'],
+});
 
-export default function ThemeProvider({children}: ThemeProviderProps) {
-  return <ConfigProvider
-    theme={{
-      token: {
-        colorPrimary: '#006D75',
-        colorLink: '#006D75',
-      }
-    }}
-  >
-    {children}
-  </ConfigProvider>
+export default function ThemeProvider( {children}: ThemeProviderProps ) {
+	return <ConfigProvider
+		theme={{
+			token: {
+				colorPrimary: '#006D75',
+				fontFamily: lexendDeca.style.fontFamily,
+				colorText: '#000e1d',
+				colorLink: '#006D75',
+			},
+			components: {
+				Card: {
+					paddingLG: 20,
+					borderRadiusLG: 0,
+					boxShadowCard: 'none'
+				},
+				Button: {
+					borderRadius: 2,
+					primaryShadow: 'none'
+				}
+			}
+		}}
+	>
+		{children}
+	</ConfigProvider>;
 }
