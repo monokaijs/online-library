fb72b60deab5e59b9760c72d62dc6caba3bb79f3
anh cao <anhct@northstudio.vn>
2024-02-23 23:40:39 +0700

    add app constant

---
+++ src/constants/app.constant.ts
@@ -0,0 +1,13 @@
+export enum Status {
+  IDLE = 'idle',
+  PENDING = 'pending',
+  FULFILLED = 'fulfilled',
+  REJECTED = 'rejected'
+}
+
+export enum FormAction {
+  VIEW = 'view',
+  CREATE = 'create',
+  UPDATE = 'update',
+  REMOVE = 'remove'
+}
