da4aaeceb12337acc1da83e5c3f4fb866743fab8
Edward Nguyen <nhanna@northstudio.vn>
2024-03-12 09:05:55 +0700

    :zap: temporarily allow missing suspense

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: false,
   experimental: {
     esmExternals: "loose",
-    serverComponentsExternalPackages: ["mongoose"]
+    serverComponentsExternalPackages: ["mongoose"],
+    missingSuspenseWithCSRBailout: false,
   },
   transpilePackages: [ "antd", "@ant-design", "rc-util", "rc-pagination", "rc-picker", "rc-notification", "rc-tooltip", "rc-tree", "rc-table" ],
 }
