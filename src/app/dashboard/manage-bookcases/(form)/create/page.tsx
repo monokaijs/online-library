94ed68761a1c4c0589912ba023886f5cca03300c
phamdngoc <ngocpd@northstudio.vn>
2024-05-06 15:30:01 +0700

    ♻️ Refactor UI

--- src/app/dashboard/manage-bookcases/(form)/create/page.tsx
+++ src/app/dashboard/manage-bookcases/(form)/create/page.tsx
@@ -1,8 +1,10 @@
 import { FormAction } from "@/constants/app.constant";
 import { libraryService } from "@/lib/services/library.service";
 import BookcaseForm from "../components/BookcaseForm";
+import { dbService } from "@/lib/services/db.service";
 
 export default async function CreateBookCasePage() {
+  await dbService.connect();
   const libraries = await libraryService.get();
   return <BookcaseForm libraries={libraries} action={FormAction.CREATE} />;
 }
