598a21ca1102d1b9d15772571682cd215c46cd37
phamdngoc <ngocpd@northstudio.vn>
2024-05-22 15:40:52 +0700

    🔨 Type fix

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -5,7 +5,7 @@ export async function GET(req: Request, res: Response) {
   await dbService.connect();
   const { searchParams } = new URL(req.url ?? "");
   const page = Number(searchParams.get("page") ?? 1);
-  const limit = Number(searchParams.get("limit") ?? 1);
+  const limit = Number(searchParams.get("limit") ?? 20);
   const library = searchParams.get("library");
   const type = searchParams.get("type");
   const query = searchParams.get("query");
