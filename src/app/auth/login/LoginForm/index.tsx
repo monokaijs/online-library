58ed23ece5416c5f3359c228aa6392c508fcc71f
Edward Nguyen <nhanna@northstudio.vn>
2024-05-23 17:37:33 +0700

    :bug: fix wrong google auth redirect_uri #58

--- src/app/auth/login/LoginForm/index.tsx
+++ src/app/auth/login/LoginForm/index.tsx
@@ -41,7 +41,7 @@ export default function LoginForm(props: LoginFormProps) {
 
   const login = useGoogleLogin({
     flow: "auth-code",
-    onSuccess: ({ code }) => gConnectAction({ code: code, type: "google" }),
+    onSuccess: ({ code }) => gConnectAction({ code: code, type: "google", redirect_uri:"postmessage" }),
     onNonOAuthError: () => {
       setLoading(false);
     },
