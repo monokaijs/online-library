29c51bb4cf76ba93cc6e4b9022a92d1b2c9fda21
Edward Nguyen <nhanna@northstudio.vn>
2024-05-23 15:49:35 +0700

    :rocket: proxy get library response

--- src/app/api/libraries/route.ts
+++ src/app/api/libraries/route.ts
@@ -4,7 +4,7 @@ import { libraryService } from "@/lib/services/library.service";
 export async function GET(req: Request, res: Response) {
   await dbService.connect();
   // TODO: Temporary fix for builds
-  if (!dbService.connected) return [];
+  if (!dbService.connected) return Response.json([]);
   const data = await libraryService.get();
   return Response.json(data);
 }
