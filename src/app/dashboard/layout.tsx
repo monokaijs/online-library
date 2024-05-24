9d648d404d8fde341190e8874e80fc253f8187d6
anh cao <anhct@northstudio.vn>
2024-05-24 22:47:54 +0700

    :bug: fix scroll horizontal layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,29 +3,32 @@ import SessionProvider from "@/components/shared/SessionContext";
 import {getSession} from "@/lib/utils/getSession";
 import {ConfigProvider} from "antd";
 import {redirect} from "next/navigation";
-import {Suspense} from "react";
 
 export default async function DashboardLayout({children}: any) {
   const session = await getSession();
   if (!session.signedIn) return redirect('/auth/login');
 
-  return <SessionProvider session={{
-    signedIn: session.signedIn,
-    account: session.account,
-  }}>
-    <ConfigProvider
-      theme={{
-        components: {
-          Input: {},
-          DatePicker: {},
-          Layout: {
-            triggerBg: "#F3F3F3"
-          }
-        }
+  return (
+    <div style={{height: "100vh", overflowX: "hidden"}}>
+      <SessionProvider session={{
+        signedIn: session.signedIn,
+        account: session.account,
       }}>
-      <DashboardLayoutContent>
-        {children}
-      </DashboardLayoutContent>
-    </ConfigProvider>
-  </SessionProvider>
+        <ConfigProvider
+          theme={{
+            components: {
+              Input: {},
+              DatePicker: {},
+              Layout: {
+                triggerBg: "#F3F3F3"
+              }
+            }
+          }}>
+          <DashboardLayoutContent>
+            {children}
+          </DashboardLayoutContent>
+        </ConfigProvider>
+      </SessionProvider>
+    </div>
+  )
 }
